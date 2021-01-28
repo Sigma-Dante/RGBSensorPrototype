07e45a97f6201cbf0438f1b3f0fe9caa37584161
Sigte <49625118+Sigma-Dante@users.noreply.github.com>
2021-01-28 13:44:39 -0700

    Commiting before applying James code

--- RGBSensor_StreamLine/RGBSensor_StreamLine.ino
+++ RGBSensor_StreamLine/RGBSensor_StreamLine.ino
@@ -391,7 +391,7 @@ void tcaselect(uint8_t i) {
   if (i > 7) return;
   Wire.beginTransmission(TCAADDR);
   Wire.write(1 << i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
 }
 
 void setExtension(int extensionSetting) {
